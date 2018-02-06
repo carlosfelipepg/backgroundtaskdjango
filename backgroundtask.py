c3821105e8404f03645c3f8dc36a83a63f195f57
carlosfelipepg <carlosfelipepg@gmail.com>
2018-02-06 09:00:39 -0300

    added py file

---
+++ backgroundtask.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+
+def your_function():
+    print("your code")
+
+
+class Execute(object):
+    #set interval time
+    def __init__(self, interval=10):
+        self.interval = interval
+
+        thread = threading.Thread(target=self.run, args=())
+        #if it is equal to false background task is not active
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+
+        while True:
+            # Do something
+            your_function()
+
+            time.sleep(self.interval)
+
+
+teste = Execute()
